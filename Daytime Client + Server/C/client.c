d43d39b37519810a9404d7f47136b2e02eca15f3
vlexum <79882639+vlexum@users.noreply.github.com>
2023-02-04 13:32:03 -0700

    Update client.c
    - handling gethostbyname()
    - error check to make sure we get IP

--- Daytime Client + Server/C/client.c
+++ Daytime Client + Server/C/client.c
@@ -10,13 +10,22 @@ int main() {
     char c;
     
     printf("Daytime client\n");
+
+    // get IP for hostname
+    struct hostent *host = gethostbyname(DAYTIME_ADDR);
+    
+    // make sure we have an IP
+    if (host == NULL){
+        perror("Error gathering host info\n");
+        exit(EXIT_FAILURE);
+    }
     
     // create an unnamed socket, and then name it
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     // create addr struct
     client_address.sin_family = AF_INET;
-    client_address.sin_addr.s_addr = inet_addr(gethostbyname(DAYTIME_ADDR));
     client_address.sin_port = htons(DAYTIME_PORT);
+    memcpy(&client_address.sin_addr, host->h_addr_list[0], host->h_length);
     
     // connect to server socket
     if (connect(client_socket, (struct sockaddr *)&client_address, sizeof(client_address)) == -1) {
