b4c12c7a950ad9bc461f2ce187997df9425a9493
vlexum <scottyaustin5@gmail.com>
2023-02-06 18:30:38 -0700

    Update DaytimeClient.java
    - Connects to government daytime server
    - new method: handleServer() - handles the input stream

--- Daytime Client + Server/Java/DaytimeClient.java
+++ Daytime Client + Server/Java/DaytimeClient.java
@@ -1,3 +1,29 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.io.InputStreamReader;
+
 public class DaytimeClient {
-    
+    public static void main(String[] args) {
+        String hostname = "time.nist.gov";
+        int port = 13;
+
+        handleServer(hostname, port);
+    }
+
+    public static void handleServer (String host, int port) {
+        try (Socket socket = new Socket(host, port)) {
+            InputStream input = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+            char letter;
+
+            while ((letter = (char) reader.read()) != '*') {
+                System.out.print(letter);
+            }
+            
+            System.out.println();
+        } catch (IOException err) {
+            System.err.println("Error: " + err.getMessage());
+        }
+    }
 }
